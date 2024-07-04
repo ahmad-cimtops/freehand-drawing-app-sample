adcf50887539e30cd0b70b15596528aac4792ef1
ahmad-cimtops <ahmad@cimtops-sglab.com>
2024-07-04 14:52:13 +0700

    Initial Commit

---
+++ freehand-drawing-app-sample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  freehand-drawing-app-sample
+//
+//  Created by Ahmad Krisman Ryuzaki on 04/07/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
